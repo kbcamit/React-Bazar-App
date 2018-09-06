33c2d60a6b297e06613f3a5a8b8686ff7ba3d51f
“kbcamit” <amitchakraborty147@gmail.com>
2018-09-06 17:05:24 +0600

    Simple Bazar App using React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
     this.state = {
       products: [
         { name: 'Burger', price: 150 },
-        { name: 'Grill', price: 79 }
+        { name: 'Vegitable', price: 79 }
       ]
     };
   }
@@ -47,22 +47,57 @@ class App extends Component {
     return (
       <div className="App">
         <div className="Bazar">
-          {this.state.products.map((product, index) => {
-            price += product.price;
-            return (
-              <div key={index}>
-                <p>Name: {product.name} - Price: {product.price} - <button onClick = {()=>this.deleteProductFromList(index)}>
-                                                                    Remove</button></p>
-              </div>
-            );
-          })}
-          <p>Total: {price}</p>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.products.map((product, index) => {
+                price += product.price;
+                return (
+                  <tr key={index}>
+                    <td>{product.name}</td>
+                    <td>{new Intl.NumberFormat().format(product.price)}</td>
+                    <td><button className="btn btn-danger" onClick = {()=>this.deleteProductFromList(index)}>X</button></td>
+                  </tr>
+                );
+              })}
+              <tr>
+                <td colSpan="2">Total Price: </td>
+                <td>{new Intl.NumberFormat().format(price)}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <form onSubmit = {this.addListToBazar}>
-          <input type="text" name="product" id="product" placeholder="Enter List Name" />
-          <input type="number" step="any" name="price" id="price" placeholder="Enter Price" />
-          <button>Add List</button>
-        </form>
+        <div className="Bazar">
+          <form onSubmit = {this.addListToBazar}>
+            <div className="form-group">
+              <div className="row">
+                <div className="col-md-4">
+                  <label>List Name:</label>
+                </div>
+                <div className="col-md-8">
+                  <input type="text" name="product" id="product" className="form-control" placeholder="Enter List Name" />
+                </div>
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="row">
+                <div className="col-md-4">
+                  <label>Price:</label>
+                </div>
+                <div className="col-md-8">
+                  <input type="number" step="any" name="price" id="price" className="form-control" placeholder="Enter Price" />
+                </div>
+              </div>
+            </div>
+            <button className="btn btn-primary">Add List</button>
+          </form>
+        </div>
       </div>
     );
   }
